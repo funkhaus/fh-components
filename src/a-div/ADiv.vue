de66a5c7c1adb599446a3d06ce39669726dfd6dd
John Robson <john@funkhaus.us>
2017-11-21 16:40:47 -0500

    Added anchor-div component

---
+++ src/a-div/ADiv.vue
@@ -0,0 +1,19 @@
+<template>
+    <router-link v-if="isRelative" class="anchor-div" :to="href" :href="href"><slot /></router-link>
+    <a v-else-if="href" class="anchor-div" :href="href" target="_blank"><slot /></a>
+    <div v-else class="anchor-div"><slot /></div>
+</template>
+
+<script>
+export default {
+    name: 'a-div',
+    props: {
+        href: String
+    },
+    computed: {
+        isRelative () {
+            return this.href && String(this.href).indexOf('/') === 0
+        }
+    }
+}
+</script>
