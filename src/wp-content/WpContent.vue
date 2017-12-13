2f52c8c7c081ddb80148521d38764528f76a4559
John Robson <john@funkhaus.us>
2017-12-13 18:32:42 -0500

    Built wp-content component, allows for dynamic template compiling

---
+++ src/wp-content/WpContent.vue
@@ -0,0 +1,24 @@
+<script>
+    import Vue from 'vue'
+
+    export default {
+        name: 'wp-content',
+        props: {
+            html: {
+                type: String,
+                default: ``
+            }
+        },
+        computed: {
+            htmlTemplate () {
+                if ( !this.html ) return `<div class="wp-content-placeholder"></div>`
+                return `<div class="wp-content-rendered">${ this.html }</div>`
+            }
+        },
+        render (h, context) {
+            const res = Vue.compile(this.htmlTemplate)
+            return res.render.apply(this, ...arguments)
+        }
+    }
+
+</script>
