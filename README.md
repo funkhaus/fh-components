1693425b9f86cded650ab0eea7633af2903104eb
Sander Moolin <sandermoolin@gmail.com>
2018-03-28 15:33:47 -0400

    Added callbacks to v-in-view

--- README.md
+++ README.md
@@ -371,9 +371,16 @@ Directives may also have "modifiers" to go with them. Modifiers can be used like
 * `above`: String, default `above-view`. Class when element is above the viewport.
 * `below`: String, default `below-view`. Class when element is below the viewport.
 * `inView`: String, default `in-view`. Class when element is within the viewport.
+* `onEnter`: Function, default `null`. Method to call when an element enters view from anywhere. Accepts one argument, the element that has entered the viewport.
+* `onEnterAbove`: Function, default `null`. Method to call when an element enters view from the top of the screen. Accepts one argument, the element that has entered the viewport.
+* `onEnterBelow`: Function, default `null`. Method to call when an element enters view from below the screen. Accepts one argument, the element that has entered the viewport.
+* `onLeave`: Function, default `null`. Method to call when an element leaves view from anywhere. Accepts one argument, the element that has ;eft the viewport.
+* `onLeaveAbove`: Function, default `null`. Method to call when an element leaves view, going above the screen. Accepts one argument, the element that has left the viewport.
+* `onLeaveBelow`: Function, default `null`. Method to call when an element leaves view, going below the screen. Accepts one argument, the element that has left the viewport.
 
 **Notes**
 * Applies classes to an element based on its viewport visibility.
+* Can also fire callbacks when an element has entered or left the viewport.
 
 ## `reverse-hover`
 **Arguments**
--- src/v-in-view/InView.vue
+++ src/v-in-view/InView.vue
@@ -18,20 +18,30 @@
         height = el.clientHeight || 0
     }
 
-    function setClass(el, opts) {
+    function setClass(el, opts, runEvents = true) {
         const possibleClasses = Object.values(opts)
         const currentClass = [...el.classList].find(className => possibleClasses.includes(className)) || ''
         let newClass = currentClass
+        let eventType = []
 
         // In-view status
         if ( height && top + height <= 0 ){
             newClass = opts.aboveClass
+            eventType.push('leave-view', 'above')
         }
         else if ( top >= window.innerHeight ){
             newClass = opts.belowClass
+            eventType.push('leave-view', 'below')
         }
         else {
             newClass = opts.inViewClass
+            eventType.push('enter-view')
+
+            if( currentClass == opts.aboveClass ){
+                eventType.push('above')
+            } else {
+                eventType.push('below')
+            }
         }
 
         // Add class if in view
@@ -40,6 +50,36 @@
                 el.classList.remove(currentClass)
             }
             el.classList.add(newClass)
+
+            // Fire events
+            if( runEvents ){
+
+                // enter events
+                if( eventType.indexOf('enter-view') != -1 ){
+                    if( opts.onEnter ){
+                        opts.onEnter(el)
+                    }
+                    if( eventType.indexOf('above') != -1 && opts.onEnterAbove ){
+                        opts.onEnterAbove(el)
+                    }
+                    if( eventType.indexOf('below') != -1 && opts.onEnterBelow ){
+                        opts.onEnterBelow(el)
+                    }
+                }
+
+                // leave events
+                if( eventType.indexOf('leave-view') != -1 ){
+                    if( opts.onLeave ){
+                        opts.onLeave(el)
+                    }
+                    if( eventType.indexOf('above') != -1 && opts.onLeaveAbove ){
+                        opts.onLeaveAbove(el)
+                    }
+                    if( eventType.indexOf('below') != -1 && opts.onLeaveBelow ){
+                        opts.onLeaveBelow(el)
+                    }
+                }
+            }
         }
     }
 
@@ -49,19 +89,33 @@
             const opts = {
                 aboveClass: bindingValue.above || 'above-view',
                 inViewClass: bindingValue.inView || 'in-view',
-                belowClass: bindingValue.below || 'below-view'
+                belowClass: bindingValue.below || 'below-view',
+                throttle: bindingValue.hasOwnProperty('throttle') ? bindingValue.throttle : 30,
+
+                // events
+                onEnter: bindingValue.onEnter || null,
+                onEnterAbove: bindingValue.onEnterAbove || null,
+                onEnterBelow: bindingValue.onEnterBelow || null,
+                onLeave: bindingValue.onLeave || null,
+                onLeaveAbove: bindingValue.onLeaveAbove || null,
+                onLeaveBelow: bindingValue.onLeaveBelow || null,
             }
 
             window.addEventListener('scroll', throttle(() => {
                 setHeight(el, opts)
                 setRect(el, opts)
                 setClass(el, opts)
-            }, 30))
+            }, opts.throttle))
             window.addEventListener('resize', throttle(() => {
                 setHeight(el, opts)
                 setRect(el, opts)
                 setClass(el, opts)
-            }, 30))
+            }, opts.throttle))
+
+            // initial class
+            setHeight(el, opts)
+            setRect(el, opts)
+            setClass(el, opts, false)
         }
     }
 
