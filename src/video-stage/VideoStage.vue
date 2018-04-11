60328a8fc7e037da2c6c55e737dd1855f12762a5
Sander Moolin <sandermoolin@gmail.com>
2018-04-11 13:53:11 -0400

    Added muted and paused props to video-stage, documented changes

--- src/video-stage/VideoStage.vue
+++ src/video-stage/VideoStage.vue
@@ -10,100 +10,122 @@
 </template>
 
 <script>
-    import fitToParent from 'fit-to-parent'
-    import Player from '@vimeo/player'
-    import Vue from 'vue'
+import fitToParent from 'fit-to-parent'
+import Player from '@vimeo/player'
+import Vue from 'vue'
 
-    export default {
-        name: 'video-stage',
-        props: {
-            src: {
-                type: String,
-                default: ''
-            },
-            autoplay: {
-                type: [Boolean, String, Number],
-                default: true
-            },
-            autoOffsets: {
-                type: [Boolean, String, Number],
-                default: true
-            }
+export default {
+    name: 'video-stage',
+    props: {
+        src: {
+            type: String,
+            default: ''
         },
-        data () {
-            return {
-                iframeCode: '',
-                loadingEmbed: false,
-                player: null
-            }
+        autoplay: {
+            type: [Boolean, String, Number],
+            default: true
         },
-        watch: {
-            src () { this.newSource() },
-            iframeCode (embed) {
-                if ( embed && embed.includes('vimeo.com') ){
-                    Vue.nextTick(this.initVimeoPlayer)
-                }
-                Vue.nextTick(this.sizeVideo)
-            }
+        autoOffsets: {
+            type: [Boolean, String, Number],
+            default: true
         },
-        mounted () {
+        paused: {
+            type: Boolean,
+            default: false
+        },
+        muted: {
+            type: Boolean,
+            default: false
+        }
+    },
+    data() {
+        return {
+            iframeCode: '',
+            loadingEmbed: false,
+            player: null
+        }
+    },
+    watch: {
+        src() {
             this.newSource()
-            window.addEventListener('resize', this.sizeVideo)
         },
-        beforeDestroy () {
-            this.destroyVimeoPlayer()
-            window.removeEventListener('resize', this.sizeVideo)
+        iframeCode(embed) {
+            if (embed && embed.includes('vimeo.com')) {
+                Vue.nextTick(this.initVimeoPlayer)
+            }
+            Vue.nextTick(this.sizeVideo)
         },
-        computed: {
-            isIframe () {
-                return String(this.src).includes('<iframe')
-            },
-            embedIsVimeo () {
-                return this.parsedEmbed && this.parsedEmbed.includes('vimeo.com')
+        paused(newVal) {
+            if (newVal) {
+                this.player.pause()
+            } else {
+                this.player.play()
+            }
+        }
+    },
+    mounted() {
+        this.newSource()
+        window.addEventListener('resize', this.sizeVideo)
+    },
+    beforeDestroy() {
+        this.destroyVimeoPlayer()
+        window.removeEventListener('resize', this.sizeVideo)
+    },
+    computed: {
+        isIframe() {
+            return String(this.src).includes('<iframe')
+        },
+        embedIsVimeo() {
+            return this.parsedEmbed && this.parsedEmbed.includes('vimeo.com')
+        }
+    },
+    methods: {
+        onPlay() {
+            this.$emit('play', ...arguments)
+        },
+        onPause() {
+            this.$emit('pause', ...arguments)
+        },
+        onEnded() {
+            this.$emit('ended', ...arguments)
+        },
+        async newSource() {
+            if (!this.isIframe) {
+                this.iframeCode = await this.fetchOembed(this.src)
+                return this.iframeCode
+            }
+            this.iframeCode = this.src
+        },
+        async initVimeoPlayer() {
+            // init player
+            this.player = new Player(this.getIframe(), {
+                muted: this.muted
+            })
+            await this.player.ready()
+
+            // set iframe events
+            this.player.on('play', this.onPlay)
+            this.player.on('pause', this.onPause)
+            this.player.on('ended', this.onEnded)
+
+            // if set to auto, play
+            if (this.autoplay) {
+                await this.player.setVolume(0)
+                this.player.play()
             }
         },
-        methods: {
-            onPlay () { this.$emit('play', ...arguments) },
-            onPause () { this.$emit('pause', ...arguments) },
-            onEnded () { this.$emit('ended', ...arguments) },
-            async newSource () {
-                if ( !this.isIframe ) {
-                    this.iframeCode = await this.fetchOembed(this.src)
-                    return this.iframeCode
-                }
-                this.iframeCode = this.src
-            },
-            async initVimeoPlayer () {
-
-                // init player
-                this.player = new Player(this.getIframe())
-                await this.player.ready()
-
-                // set iframe events
-                this.player.on('play', this.onPlay)
-                this.player.on('pause', this.onPause)
-                this.player.on('ended', this.onEnded)
-
-                // if set to auto, play
-                if ( this.autoplay ) {
-                    await this.player.setVolume(0)
-                    this.player.play()
-                }
-            },
-            getIframe () {
-                return this.$el.querySelector && this.$el.querySelector('iframe')
-            },
-            sizeVideo () {
-                let frame
-                if ( frame = this.getIframe() ) {
-
-                    let heightOffset = 0
-                    if ( this.autoOffsets ) {
-
-                        // total height of all slot elements (only if auto is true)
-                        heightOffset = Object.keys(this.$slots).reduce((acc, key) => {
-
-                            const slotItems = (this.$slots[key] || [])
+        getIframe() {
+            return this.$el.querySelector && this.$el.querySelector('iframe')
+        },
+        sizeVideo() {
+            let frame
+            if ((frame = this.getIframe())) {
+                let heightOffset = 0
+                if (this.autoOffsets) {
+                    // total height of all slot elements (only if auto is true)
+                    heightOffset = Object.keys(this.$slots).reduce(
+                        (acc, key) => {
+                            const slotItems = this.$slots[key] || []
                             const slotHeight = slotItems.reduce((acc, item) => {
                                 const rect = item.elm.getBoundingClientRect()
                                 const height = rect.height || 0
@@ -111,59 +133,59 @@
                             }, 0)
 
                             return slotHeight
-                        }, 0)
-                    }
-
-                    fitToParent({
-                        element: frame,
-                        heightOffset
-                    })
-                }
-            },
-            destroyVimeoPlayer () {
-
-                // set iframe events
-                this.player.off('play', this.onPlay)
-                this.player.off('pause', this.onPause)
-                this.player.off('ended', this.onEnded)
-
-                this.player = null
-            },
-            isVimeo (url) {
-                return String(url).match(RegExp('https?://(.+\.)?vimeo\.com/.*'))
-            },
-            async fetchOembed (url) {
-
-                // fetch from vimeo
-                if ( this.isVimeo(url) ){
-                    this.loadingEmbed = true
-                    const base = 'https://vimeo.com/api/oembed.json'
-                    const encoded = encodeURIComponent(this.src)
-                    const response = await fetch(`${ base }?url=${ encoded }`).then(r => r.json())
-                    this.loadingEmbed = false
-                    return response && response.html ? response.html : ''
+                        },
+                        0
+                    )
                 }
 
-                // TODO: other types of oembed support here if possible
-
-                return ''
+                fitToParent({
+                    element: frame,
+                    heightOffset
+                })
             }
+        },
+        destroyVimeoPlayer() {
+            // set iframe events
+            this.player.off('play', this.onPlay)
+            this.player.off('pause', this.onPause)
+            this.player.off('ended', this.onEnded)
+
+            this.player = null
+        },
+        isVimeo(url) {
+            return String(url).match(RegExp('https?://(.+.)?vimeo.com/.*'))
+        },
+        async fetchOembed(url) {
+            // fetch from vimeo
+            if (this.isVimeo(url)) {
+                this.loadingEmbed = true
+                const base = 'https://vimeo.com/api/oembed.json'
+                const encoded = encodeURIComponent(this.src)
+                const response = await fetch(`${base}?url=${encoded}`).then(r =>
+                    r.json()
+                )
+                this.loadingEmbed = false
+                return response && response.html ? response.html : ''
+            }
+
+            // TODO: other types of oembed support here if possible
+
+            return ''
         }
     }
+}
 </script>
 
 <style lang="scss">
-
-    .video-stage-module {
-        position: absolute;
-        justify-content: center;
-        flex-direction: column;
-        align-items: center;
-        display: flex;
-        bottom: 0;
-        right: 0;
-        left: 0;
-        top: 0;
-    }
-
+.video-stage-module {
+    position: absolute;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    display: flex;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    top: 0;
+}
 </style>
