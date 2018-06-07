eb60fd223e67c27501476ab431e25b83f80026ae
Sander <sander@funkhaus.us>
2018-06-07 15:07:22 -0400

    Fixed a bug with multiple keydown listeners on v-keydown

--- src/v-keydown/Keydown.vue
+++ src/v-keydown/Keydown.vue
@@ -1,13 +1,16 @@
 <script>
 // we can only add and remove listeners for named functions, not anonymous ones,
-// so this is where we'll store the named function
-let func = null
+// so this is where we'll store the named functions
+let funcs = {}
 
 export default {
     bind: (el, { modifiers, value }) => {
-        func = function(evt) {
+        const modifierKeys = Object.keys(modifiers)
+        const hasModifiers = modifierKeys.length
+
+        const func = function(evt) {
             if (
-                !Object.keys(modifiers).length ||
+                !hasModifiers ||
                 (evt.keyCode == 27 && modifiers.esc) ||
                 (evt.keyCode == 38 && modifiers.up) ||
                 (evt.keyCode == 39 && modifiers.right) ||
@@ -17,10 +20,18 @@ export default {
                 value()
             }
         }
-        window.addEventListener('keydown', func)
+
+        const key = hasModifiers ? modifierKeys.join('') : 'all'
+        funcs[key] = func
+
+        window.addEventListener('keydown', funcs[key])
     },
     unbind: (el, { modifiers, value }) => {
-        window.removeEventListener('keydown', func)
+        Object.keys(funcs).map(key => {
+            window.removeEventListener('keydown', funcs[key])
+        })
+
+        funcs = {}
     }
 }
 </script>
